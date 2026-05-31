70062a3d2e76fc309bc7576f2359c23106dabf22
glowcouch <42631203+glowcouch@users.noreply.github.com>
2026-05-31 12:09:17 +1200

    feat(place_search): update record

--- place_search/place_search.js
+++ place_search/place_search.js
@@ -1,1 +1,10 @@
-document.getElementById("readout").innerHTML = "hi";
+async function update () {
+  table = grist.getTable();
+  await table.update({
+    id: 1,
+    fields: {
+      name: 'New name',
+      website: 'http://www.google.com'
+    }
+  });
+}
